f13cb669d5df90071a60521b4921cb226497f5e1
Alex Shapiro <alexander.max.shapiro@gmail.com>
2025-11-16 22:03:10 -0500

    rendered tetris

---
+++ main.py
@@ -0,0 +1,16 @@
+from pufferlib.ocean.tetris import tetris
+
+env = tetris.Tetris()
+obs, info = env.reset()
+done = False
+total_reward = 0
+
+while not done:
+    action = env.action_space.sample()
+    obs, reward, terminated, truncated, info = env.step(action)
+    frame = env.render()
+    total_reward += reward
+    done = terminated or truncated
+
+print(f"Episode finished! Total reward: {total_reward}")
+env.close()
